e655260ce714312215ec746c3f8bd6b881c0d88c
RT Flores <rotyflo@gmail.com>
2018-12-08 00:21:45 -0600

    Repeat a string repeat a string

---
+++ repeat_string_num_times.py
@@ -0,0 +1,7 @@
+def repeat_string_num_times(string, num):
+	"""Repeat a given string for num times.
+	   Return an empty string if num is not a positive number."""
+	return string * num
+
+
+print(repeat_string_num_times("abc", 3))
