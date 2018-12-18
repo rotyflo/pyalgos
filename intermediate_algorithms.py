106e7631692c56c66ab2ac6a922c48be7eb75624
RT Flores <rotyflo@gmail.com>
2018-12-18 07:42:17 -0800

    Return the difference between two arrays

--- intermediate_algorithms.py
+++ intermediate_algorithms.py
@@ -12,3 +12,14 @@ def sum_all(arr):
     return total
 
 # print(sum_all([1, 4]))
+
+
+def diff_array(arr1, arr2):
+    new_arr = []
+
+    [new_arr.append(item) for item in arr1 if item not in arr2]
+    [new_arr.append(item) for item in arr2 if item not in arr1]
+
+    return new_arr
+
+# print(diff_array([1, 2, 3, 5], [1, 2, 3, 4, 5]))
